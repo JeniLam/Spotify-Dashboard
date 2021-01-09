966f57745669197b7194e89c47e3e7225a90c8f8
JeniLam <jenilam639@gmail.com>
2021-01-09 10:57:37 -0600

    Adds clean data and SQL info

---
+++ Data/Data_Analysis.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "Top_Songs_2010-2019" (
+    "title" varchar   NOT NULL,
+    "artist" varchar   NOT NULL,
+    "top_genre" varchar   NOT NULL,
+    "year" int   NOT NULL,
+    "bpm" int   NOT NULL,
+    "energy" int   NOT NULL,
+    "dancability" int   NOT NULL,
+    "dB" int   NOT NULL,
+    "live" int   NOT NULL,
+    "valence" int   NOT NULL,
+    "duration_in_seconds" int   NOT NULL,
+    "acousticness" int   NOT NULL,
+    "speechiness" int   NOT NULL,
+    "popularity" int   NOT NULL
+);
