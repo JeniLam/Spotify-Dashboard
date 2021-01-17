c81b3e16d2a6324edac904fb1b9d8fe552c3e49c
JeniLam <jenilam639@gmail.com>
2021-01-16 20:35:22 -0600

    Starts data manipulation

--- Jeni/app1.py
+++ Jeni/app1.py
@@ -33,25 +33,25 @@ results = session.query(songs.title, songs.artist, songs.top_genre, songs.year, 
 # close session
 session.close()
 
-# # create a list of dictionaries from the row data and append to a list of all data
-all_data = []
+# # # create a list of dictionaries from the row data and append to a list of all data
+# all_data = []
 
-for title, artist, top_genre, year, energy, dancability, bpm, dB, duration_in_seconds, acousticness, popularity in results:
-        music_dict = {}
-        music_dict['title'] = title
-        music_dict['artist'] = artist
-        music_dict['top_genre'] = top_genre
-        music_dict['year'] = year
-        music_dict['energy'] = energy
-        music_dict['dancability'] = dancability
-        music_dict['bpm'] = bpm
-        music_dict['dB'] = dB
-        music_dict['duration_in_seconds'] = duration_in_seconds
-        music_dict['acousticness'] = acousticness
-        music_dict['popularity'] = popularity
-        all_data.append(music_dict)
-print(all_data[0])
-print(all_data[1])
+# for title, artist, top_genre, year, energy, dancability, bpm, dB, duration_in_seconds, acousticness, popularity in results:
+#         music_dict = {}
+#         music_dict['title'] = title
+#         music_dict['artist'] = artist
+#         music_dict['top_genre'] = top_genre
+#         music_dict['year'] = year
+#         music_dict['energy'] = energy
+#         music_dict['dancability'] = dancability
+#         music_dict['bpm'] = bpm
+#         music_dict['dB'] = dB
+#         music_dict['duration_in_seconds'] = duration_in_seconds
+#         music_dict['acousticness'] = acousticness
+#         music_dict['popularity'] = popularity
+#         all_data.append(music_dict)
+# print(all_data[0])
+# print(all_data[1])
 
 
 
@@ -68,9 +68,6 @@ def welcome():
     return (render_template("index.html")
             )
 
-# @app.route("/table1")
-# def table_1_assembly():
-#     genre = 
 
 @app.route("/api/v1.0/data")
 def all_data():
--- Jeni/static/app.js
+++ Jeni/static/app.js
@@ -1,4 +1,42 @@
 url= "/api/v1.0/data"
 console.log(url)
-d3.json(url).then((data) => {alert('Hooray')
-    console.log(data)});
+
+// create barchart
+d3.json(url).then((data) => {
+    console.log(data)
+
+    var year = []
+    var genre = []
+    var artist = []
+// Mod 14 - Day 2 exercise 8 - pull values out of object
+    data.forEach((d) => {
+        Object.entries(d).forEach(([key, value]) => {
+            if (key === "year") {
+                year.push(value)
+            }
+            else if (key ==="top_genre") {
+                genre.push(value)
+            }
+            else if (key === "artist") {
+                artist.push(value)
+            }
+        })
+    });
+
+     
+    // var trace1 = {
+    //     x = data.year,
+    //     y = data.top_genre,
+    //     type: "bar"
+    // };
+
+    // var bardata = [trace1];
+
+    // var barlayout = {
+    //     title: "Top Genre by Year",
+    //     xaxis: {title: "Year"},
+    //     yaxis: {title: "Count of Genres"}
+    // };
+
+    // Plotly.newPlot("bar", bardata, barlayout)
+});
