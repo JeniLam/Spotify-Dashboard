925963bfb9a24638a544a65dc39401631cbeb7e4
JeniLam <jenilam639@gmail.com>
2021-01-21 15:15:38 -0600

    Updates D3

--- Jeni/static/d3app.js
+++ Jeni/static/d3app.js
@@ -1,3 +1,26 @@
+
+// function makeResponsive() {
+// Add boiler plate information for SVG area, margins, etc
+var svgWidth = 900;
+var svgHeight = 600;
+
+// Define the chart's margins as an object
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+// Select scatter id to hold chart, append SVG area to it, and set its dimensions
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 // / Append a group area, then set its margins
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
